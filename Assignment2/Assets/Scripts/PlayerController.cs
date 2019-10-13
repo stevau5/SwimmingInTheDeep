bed9371cfd3f08518b04696dd58a839c818268d8
Steven <stevenanthonyzanga@gmail.com>
2019-10-13 19:51:58 -0400

    see last commit msg

---
+++ Assignment2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Reference to Rigidbody 2D
+    public Rigidbody2D playerRigidBody; 
+
+    // ref to animator
+    public Animator myAnim;
+    // speed
+    public float moveSpeed = 3.5f;
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRigidBody.velocity = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+        ) * moveSpeed;
+    }
+}
